7254ee346b32591759b2f1f704c9842a93260d89
Pasha <pasha2k@yandex.ru>
2019-06-26 13:49:28 +0300

    HW3 update: JdbcMealRepositoryImpl refactored: - ROW_MAPPER now use BeanPropertyRowMapper populateDB fix: - date renamed to date_time

--- src/main/java/ru/javawebinar/topjava/repository/jdbc/JdbcMealRepositoryImpl.java
+++ src/main/java/ru/javawebinar/topjava/repository/jdbc/JdbcMealRepositoryImpl.java
@@ -3,8 +3,8 @@ package ru.javawebinar.topjava.repository.jdbc;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.dao.support.DataAccessUtils;
+import org.springframework.jdbc.core.BeanPropertyRowMapper;
 import org.springframework.jdbc.core.JdbcTemplate;
-import org.springframework.jdbc.core.RowMapper;
 import org.springframework.jdbc.core.namedparam.MapSqlParameterSource;
 import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
 import org.springframework.jdbc.core.simple.SimpleJdbcInsert;
@@ -20,14 +20,7 @@ import java.util.List;
 @Qualifier("jdbc")
 public class JdbcMealRepositoryImpl implements MealRepository {
 
-    private static final RowMapper<Meal> ROW_MAPPER = (resultSet, i) -> {
-        Meal meal = new Meal();
-        meal.setId(resultSet.getInt("id"));
-        meal.setDateTime(resultSet.getTimestamp("date_time").toLocalDateTime());
-        meal.setDescription(resultSet.getString("description"));
-        meal.setCalories(resultSet.getInt("calories"));
-        return meal;
-    };
+    private static final BeanPropertyRowMapper<Meal> ROW_MAPPER = BeanPropertyRowMapper.newInstance(Meal.class);
 
     private final JdbcTemplate jdbcTemplate;
 
