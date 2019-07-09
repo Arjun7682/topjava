6f7e8fcc7a81de8b9702fc3683aa985c220c49fa
Pasha <pasha2k@yandex.ru>
2019-07-09 19:00:50 +0300

    HW5: Add test for all implementations (jdbc, jpa, datajpa)

---
+++ src/test/java/ru/javawebinar/topjava/service/DataJpaUserServiceTest.java
@@ -0,0 +1,9 @@
+package ru.javawebinar.topjava.service;
+
+import org.springframework.test.context.ActiveProfiles;
+
+@ActiveProfiles({
+        "datajpa",
+        "datajpa,jpa"})
+public class DataJpaUserServiceTest extends UserServiceTest {
+}
