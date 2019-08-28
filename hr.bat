d4a766f4065cc951e23484e6e795c8827d046c01
Pasha <pasha2k@yandex.ru>
2019-08-28 15:21:05 +0300

    heroku

--- hr.bat
+++ hr.bat
@@ -1,2 +1,2 @@
 call mvn -B -s settings.xml -DskipTests=true clean package
-call java -Dspring.profiles.active="datajpa,heroku" -DDATABASE_URL="postgres://user:password@localhost:5432/topjava" -jar target/dependency/webapp-runner.jar target/*.war
+call java -Dspring.profiles.active="datajpa,heroku" -DDATABASE_URL="postgresql://ec2-79-125-126-205.eu-west-1.compute.amazonaws.com:5432/dalpsq4q5tl3kg" -jar target/dependency/webapp-runner.jar target/*.war
